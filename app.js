a6d00b43fa6863357fd0a20054380e717debe29a
imsunhao <sunhao__159@163.com>
2017-01-06 17:11:01 +0800

    start git

---
+++ app.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var mongoDBConfig=require('./config/mongoBDConfig.json');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+mongoose.connect('mongodb://'+mongoDBConfig.user+':'+mongoDBConfig.password+'@'+mongoDBConfig.host+':'+mongoDBConfig.port+'/'+mongoDBConfig.database+'?authSource='+mongoDBConfig.authSource, {native_parser: true});
+// mongoose.connect('mongodb://localhost:18088/datas');
+
+mongoose.connection.on('error', console.error.bind(console, '连接数据库失败'));
+app.use(session({
+    key: 'session',
+    secret: 'keboard cat',
+    cookie: {maxAge: 1000 * 60 * 60 * 24},//1day
+    store: new MongoStore({
+        db: 'datas',
+        mongooseConnection: mongoose.connection
+    }),
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,220 @@
+var express = require('express');
+var mongoose = require('mongoose');
+//引入加密模块
+var crypto = require('crypto');
+
+var model = require('../models/model');
+
+var User = model.User;
+var Article = model.Article;
+
+var router = express.Router();
+
+var secret='a12345678';
+
+/*Set routerParam*/
+router.param('_id', function (req, res, next, id) {
+    console.log('CALLED ONLY ONCE');
+    next();
+});
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: '主页',
+        arts: [{
+            title: 'nodeJS入门',
+            tags: 'nodeJS',
+            author: 'imsunhao',
+            createTime: '2016-12-27 10:45:01',
+            content: '巴拉巴拉'
+        }, {
+            title: 'nodeJS入门',
+            tags: 'nodeJS',
+            author: 'imsunhao',
+            createTime: '2016-12-27 10:45:01',
+            content: '巴拉巴拉'
+        }, {
+            title: 'nodeJS入门',
+            tags: 'nodeJS',
+            author: 'imsunhao',
+            createTime: '2016-12-27 10:45:01',
+            content: '巴拉巴拉'
+        }],
+        user: req.session.user
+    });
+});
+
+router.get('/login', function (req, res, next) {
+    res.render('login', {title: 'login'});
+});
+
+router.post('/login',function (req,res,next) {
+    var username = req.body.email,
+        password = req.body.password;
+    //检查用户名是否已经存在
+    //mongoose findOne() 方法
+    User.findOne({username: username}, function (err, user) {
+        if (err) {
+            console.log(err);
+            // return res.redirect('/login');
+            return res.render('login', {title: 'login'});
+        }
+        if (user) {
+            //对密码进行md5加密
+            var md5password=crypto.createHash('md5',secret).update(password).digest('hex');
+            if(md5password===user.password){
+                console.log('登陆成功！');
+                var User=user;
+                User.password = null;
+                delete User.password;
+                req.session.user = User;
+                return res.redirect('/');
+            }else {
+                console.log('password erro！'+md5password);
+                return res.redirect('/login');
+            }
+        }else{
+            console.log('用户名不存在！');
+            return res.redirect('/login');
+        }
+    });
+});
+
+router.get('/logout', function (req, res, next) {
+    req.session.destroy();
+    res.redirect('/');
+});
+
+router.get('/reg', function (req, res, next) {
+    res.render('register', {title: 'login'});
+});
+
+router.post('/reg', function (req, res, next) {
+    var username = req.body.email,
+        password = req.body.password;
+        // passwordRepeat = req.body.passwordRepeat;
+
+    //检查两次输入的密码是否一致
+    // if (password != passwordRepeat) {
+    //     console.log('两次输入的密码不一致！');
+    //     return res.redirect('/reg');
+    // }
+
+    //检查用户名是否已经存在
+    //mongoose findOne() 方法
+    User.findOne({username: username}, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.redirect('/reg');
+        }
+        if (user) {
+            console.log('用户名已经存在');
+            return res.redirect('/reg');
+        }
+        //对密码进行md5加密
+        var md5password = crypto.createHash('md5',secret).update(password).digest('hex');
+        var newUser = new User({
+            username: username,
+            password: md5password
+        });
+        //mongoose save()方法
+        newUser.save(function (err, doc) {
+            if (err) {
+                console.log(err);
+                return res.redirect('/reg');
+            }
+            console.log('注册成功！');
+            newUser.password = null;
+            delete newUser.password;
+            req.session.user = newUser;
+            return res.redirect('/');
+
+        });
+    });
+});
+
+router.get('/post', function (req, res, next) {
+    res.render('post', {title: 'login'});
+});
+
+router.post('/post', function (req, res, next) {
+    var data = new Article({
+        title: req.body.title,
+        author: req.session.user.username,
+        tag: req.body.tag,
+        content: req.body.content
+    });
+
+    data.save(function (err, doc) {
+        if (err) {
+            req.flash('error', err);
+            return res.redirect('/post');
+        }
+        console.log('文章发表成功！');
+        return res.redirect('/');
+    });
+});
+
+router.get('/search', function(req, res, next) {
+    //req.query 获取 get 请求的参数，并构造为正则对象
+    var query = req.query.title,
+        title = new RegExp(query, 'i');
+    Article
+        .find({title: title})
+        .sort('-createTime')
+        .exec(function(err, arts) {
+            if(err) {
+                console.loh(err);
+                return res.redirect('/');
+            }
+            res.render('search', {
+                title: '查询结果',
+                arts: arts
+            });
+        });
+});
+
+router.get('/edit/:_id', function(req, res, next) {
+    Article.findOne({_id: req.params._id}, function(err, art) {
+        if(err) {
+            console.log(err);
+            return res.redirect('back');
+        }
+        res.render('edit', {
+            title: '编辑',
+            art: art
+        });
+    });
+});
+
+router.post('/edit/:_id', function(req, res, next) {
+    //mongoose 的 update() 方法用过检索参数并返回修改结果
+    Article.update({_id: req.params._id},{
+        title: req.body.title,
+        tag: req.body.tag,
+        content: req.body.content,
+        createTime: Date.now()
+    }, function(err, art) {
+        if(err) {
+            console.log(err);
+            return res.redirect('back');
+        }
+        console.log('文章编辑成功！');
+        return res.redirect('/u/' + req.session.user.username);
+    });
+});
+
+router.get('/remove/:_id', function (req, res, next) {
+    //req.params 处理 /:xxx 形式的 get 或 post 请求，获取请求参数
+    Article.remove({_id: req.params._id}, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('文章删除成功！');
+        }
+        return res.redirect('back');
+    })
+});
+
+module.exports = router;
