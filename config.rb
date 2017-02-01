c61913166c98587b1c95e089cef559d738d58835
imsunhao <sunhao__159@163.com>
2017-02-01 20:24:36 +0800

    发布文章 修正样式
    下一个目标
        0.
        1.弄懂SBDoc的运行机制
        2.研究如何使用mongodb中的mongoose，进行数据库高级使用方法。
        3.测试SBDoc文档

---
+++ config.rb
@@ -0,0 +1,15 @@
+require 'compass/import-once/activate'
+require 'compass'
+require 'bootstrap-sass'
+
+http_path = "/"
+css_dir = "public/stylesheets"
+sass_dir = "sass"
+images_dir = "public/images"
+javascripts_dir = "public/javascripts"
+
+output_style =  :compressed
+# relative_assets = true
+line_comments = false
+# cache = false
+sourcemap = true
