f7a80119229890c4aaf0177be7967c6456e6e247
imsunhao <sunhao__159@163.com>
2017-02-03 09:50:10 +0800

    调整sass的输出环境 为测试环境
    下一个目标
        0.
        1.弄懂SBDoc的运行机制
        2.研究如何使用mongodb中的mongoose，进行数据库高级使用方法。
        3.测试SBDoc文档

--- config.rb
+++ config.rb
@@ -2,16 +2,16 @@ require 'compass/import-once/activate'
 require 'compass'
 require 'bootstrap-sass'
 
-http_path = "/"
-css_dir = "public/stylesheets"
-sass_dir = "sass"
-images_dir = "public/images"
-javascripts_dir = "public/javascripts"
+# http_path = "/"
+# css_dir = "public/stylesheets"
+# sass_dir = "sass"
+# images_dir = "public/images"
+# javascripts_dir = "public/javascripts"
 
-# css_dir = "viewTest/stylesheets"
-# sass_dir = "viewTest/sass"
-# images_dir = "viewTest/images"
-# javascripts_dir = "viewTest/javascripts"
+css_dir = "viewTest/stylesheets"
+sass_dir = "viewTest/sass"
+images_dir = "viewTest/images"
+javascripts_dir = "viewTest/javascripts"
 
 
 output_style =  :compressed
