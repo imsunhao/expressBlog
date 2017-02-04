b7938547a8ce8b6edfbff2d00adcd01af0ae9164
imsunhao <sunhao__159@163.com>
2017-02-04 15:02:53 +0800

    发布文章 完成路由迁移，现在根据不在index中 掺杂文章的路由控制
    下一个目标
        0.完善编辑与发布页面
        1.测试使用VueJS作为前端开发库
        2.研究如何使用mongodb中的mongoose，进行数据库高级使用方法。
        3.弄懂SBDoc的运行机制，测试SBDoc文档

--- routes/article.js
+++ routes/article.js
@@ -70,4 +70,98 @@ router.post('/articleDetails/:_id',function (req,res,next) {
     });
 });
 
+router.get('/post', function (req, res, next) {
+    res.render('article/post', {title: 'login'});
+});
+
+router.post('/post', function (req, res, next) {
+    var data = new Article({
+        title: req.body.title,
+        author: req.session.user.username,
+        tag: req.body.tag,
+        content: req.body.content
+    });
+
+    data.save(function (err, doc) {
+        if (err) {
+            req.flash('error', err);
+            return res.redirect('/article/post');
+        }
+        console.log('文章发表成功！');
+        return res.redirect('/');
+    });
+
+});
+
+router.get('/search', function(req, res, next) {
+    //req.query 获取 get 请求的参数，并构造为正则对象
+    var query = req.query.title,
+        title = new RegExp(query, 'i');
+    Article
+        .find({title: title,author:req.session.user.username})
+        .sort('-createTime')
+        .exec(function(err, arts) {
+            if(err) {
+                console.loh(err);
+                return res.redirect('/');
+            }
+            res.render('article/search', {
+                title: '查询结果',
+                arts: arts
+            });
+        });
+});
+
+router.get('/edit/:_id', function(req, res, next) {
+    Article.findOne({_id: req.params._id}, function(err, art) {
+        if(err) {
+            console.log(err);
+            return res.redirect('back');
+        }
+
+        if(req.session.user.username==art.author){
+            return res.render('article/edit', {
+                title: '编辑',
+                art: art
+            });
+        }else{
+            return res.render('error',{
+                message: '私人文章',
+                error: {
+                    status: 403,
+                    stack: "您没有修改权限呢！"
+                }})
+        }
+
+    });
+});
+
+router.post('/edit/:_id', function(req, res, next) {
+    //mongoose 的 update() 方法用过检索参数并返回修改结果
+    Article.update({_id: req.params._id},{
+        title: req.body.title,
+        tag: req.body.tag,
+        content: req.body.content
+    }, function(err, art) {
+        if(err) {
+            console.log(err);
+            return res.redirect('back');
+        }
+        console.log('文章编辑成功！');
+        return res.redirect('/article/search');
+    });
+});
+
+router.get('/remove/:_id', function (req, res, next) {
+    //req.params 处理 /:xxx 形式的 get 或 post 请求，获取请求参数
+    Article.remove({_id: req.params._id}, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('文章删除成功！');
+        }
+        return res.redirect('back');
+    })
+});
+
 module.exports = router;
