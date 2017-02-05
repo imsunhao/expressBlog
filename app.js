edfa52404da079a12344376d7ffbe3c10e2c6954
imsunhao <sunhao__159@163.com>
2017-02-05 14:28:32 +0800

    添加注释
    下一个目标
        0.美化搜索页面
        1.测试使用VueJS作为前端开发库
        2.研究如何使用mongodb中的mongoose，进行数据库高级使用方法。
        3.弄懂SBDoc的运行机制，测试SBDoc文档

--- app.js
+++ app.js
@@ -1,57 +1,196 @@
-var FileStreamRotator = require('file-stream-rotator');
+/*****************************************************************************/
+/*
+ /* 完善注释 2017-02-05 12:59:24   author:imsunhao
+ /*
+ /*****************************************************************************/
+
+/*****************************************************************************/
+/*
+ /* 启用文件系统模块
+ /*     功能                       文件系统            核心
+ /*
+ */
 var fs = require('fs');
-var express = require('express');
+/*
+ /*****************************************************************************/
+
+
+/*****************************************************************************/
+/*
+ /* 引入文件系统-路径模块
+ /*     功能            被           所有文件系统            依赖
+ /*
+ */
 var path = require('path');
-// var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var mongoDBConfig=require('./config/mongoBDConfig.json');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
-
-// var ua = require('universal-analytics');
-// var visitor = ua('UA-91300868-1');//谷歌统计
+/*
+ /*****************************************************************************/
 
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var article = require('./routes/article');
-var meal = require('./routes/meal');
-var haibinSystem = require('./routes/haibinSystem');
-
+/*****************************************************************************/
+/*
+ /* 启用express框架
+ /*     功能            核心框架
+ /*     功能            引入父系统 - index
+ /*     功能            引入子系统 - 其他
+ /*
+ */
+var express = require('express');
 var app = express();
 
-var logDirectory = path.join(__dirname, 'log');
+var index = require('./routes/index');                                   //父系统
+var users = require('./routes/users');                                   //所有系统的用户管理系统
+var article = require('./routes/article');                                //博客系统
+var meal = require('./routes/meal');                                    //吃饭系统
+var haibinSystem = require('./routes/haibinSystem');       //海滨网路系统
+/*
+ /*****************************************************************************/
 
+
+
+
+/*****************************************************************************/
+/*
+ /* 启用文件系统-读取模块
+ /*     功能            被           文件系统-记录log            依赖
+ /*
+ */
+var FileStreamRotator = require('file-stream-rotator');
+/*
+ /*****************************************************************************/
+
+
+/*****************************************************************************/
+/*
+ /* 启用文件系统-记录log
+ /*     功能            记录访问服务器日志
+ /*     网址：     https://github.com/expressjs/morgan
+ */
+var logger = require('morgan');
+
+var logDirectory = path.join(__dirname, 'log');         //日志系统路径
 var accessLogStream = FileStreamRotator.getStream({
     date_format: 'YYYYMMDD',
     filename: path.join(logDirectory, 'access-%DATE%.log'),
     frequency: 'daily',
     verbose: false
 });
+/*
+ /*****************************************************************************/
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+/*****************************************************************************/
+/*
+ /* 启用网站功能-图标
+ /*     功能            暂时未学习
+ /*     网址：
+ */
+var favicon = require('serve-favicon');
+/*
+ /*****************************************************************************/
 
-// logger :  https://github.com/expressjs/morgan
-app.use(logger('combined', {stream: accessLogStream}));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+/*****************************************************************************/
+/*
+ /* 启用网站请求分析系统-session
+ /*     功能            解析与生成    请求中    session
+ /*     网址：
+ */
+var session = require('express-session');
+/*
+ /*****************************************************************************/
+
+/*****************************************************************************/
+/*
+ /* 启用网站请求分析系统-cookie
+ /*     功能            解析与生成    请求中    cookie
+ /*     网址：
+ */
+var cookieParser = require('cookie-parser');
+/*
+ /*****************************************************************************/
+
+/*****************************************************************************/
+/*
+ /* 启用网站请求分析系统-require
+ /*     功能            解析    请求中     所有的参数
+ /*     网址：
+ */
+var bodyParser = require('body-parser');
+/*
+ /*****************************************************************************/
+
+/*****************************************************************************/
+/*
+ /* 启用网站数据存储系统-mongoose
+ /*     功能            数据库     核心
+ /*     网址：
+ */
+var mongoose = require('mongoose');
+
+
+var mongoDBConfig=require('./config/mongoBDConfig.json');       //网络数据库 配置
+var MongoStore = require('connect-mongo')(session);                      //提供 session  数据库依赖
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://'+mongoDBConfig.user+':'+mongoDBConfig.password+'@'+mongoDBConfig.host+':'+mongoDBConfig.port+'/'+mongoDBConfig.database+'?authSource='+mongoDBConfig.authSource, {native_parser: true});
-// mongoose.connect('mongodb://localhost:18088/datas');
+/*
+ /*****************************************************************************/
+
+
+/*****************************************************************************/
+/*
+ /* 启用网站功能-访问统计
+ /*     功能            统计    所有请求    的历史纪录与报表
+ /*     网址（需要翻墙）：
+ */
+// var ua = require('universal-analytics');
+// var visitor = ua('UA-91300868-1');//谷歌统计
+/*
+ /*****************************************************************************/
+
+
+/*****************************************************************************/
+/*
+ /* 启用网站view engine
+ /*     功能            被           所有视图系统            依赖
+ /*
+ */
+app.set('views', path.join(__dirname, 'views'));        //模板根路径
+app.set('view engine', 'ejs');                                     //模板为ejs模板
+/*
+ /*****************************************************************************/
+
+
+/*****************************************************************************/
+/*
+ /* 启用 数据库-链接
+ /*     功能            被           所有视图系统            依赖
+ /*
+ */
+
+// mongoose.connect('mongodb://'+mongoDBConfig.user+':'+mongoDBConfig.password+'@'+mongoDBConfig.host+':'+mongoDBConfig.port+'/'+mongoDBConfig.database+'?authSource='+mongoDBConfig.authSource, {native_parser: true});//网路数据库
+mongoose.connect('mongodb://localhost:18088/datas');         //本地测试数据库链接
 
 mongoose.connection.on('error', console.error.bind(console, '连接数据库失败'));
+/*
+ /*****************************************************************************/
+
+
+
+/*****************************************************************************/
+/*
+ /* 启用 express 中间件
+ /*     功能           见注释
+ /*
+ */
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));   //图标
+app.use(logger('combined', {stream: accessLogStream}));         //日志
+app.use(bodyParser.json());                                                         //请求解析 为json格式
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());                                                                //请求解析 cookie
+app.use(express.static(path.join(__dirname, 'public')));              //加载public资源
+/*
+ /*****************************************************************************/
+
 
 /*session*/
 app.use(session({
